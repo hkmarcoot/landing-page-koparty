263fcef81ade291f357a25b0b12085876163b466
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-01-29 02:14:48 +0000

    add files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # landing-page-koparty
+
+- Live Site URL: [Live site URL here](https://hkmarcoot.github.io/landing-page-koparty)
+
+### Screenshot
+
+![](./screenshot/koparty-desktop.png)
---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  //   alert("123");
+  $(".flexslider").flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 300,
+    itemMargin: 20,
+  });
+});
